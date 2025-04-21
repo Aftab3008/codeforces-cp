a128de09b971e0cc921a5c49f3af4cd9f8a271a2
Aftab3008 <shaikaftab005@mail.com>
2025-04-21 15:02:56 +0530

    Practice basic rated

---
+++ CF_2033_A.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CF_2033_A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int x = 0;
+            int i = 1;
+
+            while (Math.abs(x) <= n) {
+                int move = 2 * i - 1;
+
+                if (i % 2 == 1) {
+                    x -= move;
+                } else {
+                    x += move;
+                }
+
+                if (Math.abs(x) > n) {
+                    if (i % 2 == 1) {
+                        System.out.println("Sakurako");
+                    } else {
+                        System.out.println("Kosuke");
+                    }
+                    break;
+                }
+
+                i++;
+            }
+        }
+
+        scanner.close();
+    }
+}
---
+++ CF_2092_C.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+//Wrong
+public class CF_2092_C {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        while (t-- > 0) {
+            int n = input.nextInt();
+            int[] arr = new int[n];
+            ArrayList<Integer> even = new ArrayList<>();
+            ArrayList<Integer> odd = new ArrayList<>();
+            long sum = 0;
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                arr[i] = input.nextInt();
+                sum += arr[i];
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                if (arr[i] % 2 == 0) {
+                    even.add(arr[i]);
+                } else {
+                    odd.add(arr[i]);
+                }
+            }
+            if (even.isEmpty() || odd.isEmpty()) {
+                System.out.println(max);
+                continue;
+            }
+            if (n == 2) {
+                System.out.println(sum);
+                continue;
+            }
+            Collections.sort(even);
+            Collections.sort(odd);
+            int minEven = even.get(0);
+            int minOdd = odd.get(0);
+            long candidate = sum - Math.min(minEven, minOdd);
+            System.out.println(Math.max(max, candidate));
+        }
+        input.close();
+    }
+}
